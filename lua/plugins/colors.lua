d48374ed41ffc83361751244c81f92ad7c7d66b8
Sina Karimi <si.karimi1378@gmail.com>
2026-05-12 13:07:00 -0400

    Black background

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,21 +1,33 @@
-local function enable_transparency()
-    vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-end
 return {
     {
-	"folke/tokyonight.nvim",
-	config = function()
-	    vim.cmd.colorscheme "tokyonight"
-	   -- enable_transparency()
-	end
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            style = "night",
+            on_highlights = function(hl, c)
+                hl.Normal = { bg = "#000000" }
+                hl.NormalNC = { bg = "#000000" }
+                hl.NormalFloat = { bg = "#000000" }
+                hl.FloatBorder = { bg = "#000000" }
+                hl.LineNr = { bg = "#000000" }
+                hl.CursorLineNr = { bg = "#000000" }
+                hl.NeoTreeNormal = { bg = "#000000" }
+                hl.NeoTreeNormalNC = { bg = "#000000" }
+            end,
+        },
+        config = function(_, opts)
+            require("tokyonight").setup(opts)
+            vim.cmd.colorscheme "tokyonight"
+        end
     },
     {
-	"nvim-lualine/lualine.nvim",
-	dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	},
-	opts = {
-	    theme = 'tokyonight',
-	}
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            theme = 'tokyonight',
+        }
     },
 }
