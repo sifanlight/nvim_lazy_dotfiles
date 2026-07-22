40658e987c247ebd92893fda0688942ae1d39803
Sina Karimi <si.karimi1378@gmail.com>
2026-07-21 22:17:09 -0400

    fix: restore default tokyonight-night theme background

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -5,20 +5,10 @@ return {
         priority = 1000,
         opts = {
             style = "night",
-            on_highlights = function(hl, c)
-                hl.Normal = { bg = "#000000" }
-                hl.NormalNC = { bg = "#000000" }
-                hl.NormalFloat = { bg = "#000000" }
-                hl.FloatBorder = { bg = "#000000" }
-                hl.LineNr = { bg = "#000000" }
-                hl.CursorLineNr = { bg = "#000000" }
-                hl.NeoTreeNormal = { bg = "#000000" }
-                hl.NeoTreeNormalNC = { bg = "#000000" }
-            end,
         },
         config = function(_, opts)
             require("tokyonight").setup(opts)
-            vim.cmd.colorscheme "tokyonight"
+            vim.cmd.colorscheme "tokyonight-night"
         end
     },
     {
@@ -27,7 +17,7 @@ return {
             "nvim-tree/nvim-web-devicons",
         },
         opts = {
-            theme = 'tokyonight',
+            theme = "tokyonight",
         }
     },
 }
