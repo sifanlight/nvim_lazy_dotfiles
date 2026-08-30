2bfd7f2861dcf9c80c29329c67a61fe8f68169c1
Sina Karimi <si.karimi1378@gmail.com>
2026-08-30 00:04:45 -0400

    Configure system clipboard (unnamedplus)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,7 +3,5 @@ vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 
-
-
-
-
+-- Use the system clipboard for all yank/delete/put operations
+vim.opt.clipboard = "unnamedplus"
