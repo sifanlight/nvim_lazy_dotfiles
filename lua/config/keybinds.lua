12321753f95bda374c429eb01bfeb511ed55a7a6
Sina Karimi <si.karimi1378@gmail.com>
2026-09-23 14:49:54 -0400

    New obsidian plugin

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,29 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+
+-- VSCode-like Keybindings
+local map = vim.keymap.set
+
+-- Save file
+map({"n", "i", "v"}, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+
+-- Toggle Explorer / Sidebar
+map("n", "<C-b>", "<cmd>Neotree toggle<cr>", { desc = "Toggle Explorer" })
+
+-- Quick Open / Find Files
+map("n", "<C-p>", "<cmd>Telescope find_files<cr>", { desc = "Quick Open" })
+
+-- Find in Files / Search
+map("n", "<C-f>", "<cmd>Telescope live_grep<cr>", { desc = "Find in Files" })
+
+-- Close Buffer (similar to Close Editor)
+map("n", "<C-w>", "<Plug>vem_delete_buffer-", { silent = true, remap = true, desc = "Close Buffer" })
+
+-- Formatting (VSCode Option+Shift+F or Shift+Alt+F)
+map("n", "<leader>F", vim.lsp.buf.format, { desc = "Format Document" })
+
+-- Window Navigation (since C-w is used for closing buffers)
+map("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+map("n", "<C-j>", "<C-w>j", { desc = "Move to lower window" })
+map("n", "<C-k>", "<C-w>k", { desc = "Move to upper window" })
+map("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,14 @@ vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+-- VSCode-like behavior
+vim.opt.mouse = "a"           -- Enable mouse mode
+vim.opt.signcolumn = "yes"    -- Always show the signcolumn, otherwise it would shift the text each time
+vim.opt.termguicolors = true  -- True color support
+vim.opt.ignorecase = true     -- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.smartcase = true
 
 -- Use the system clipboard for all yank/delete/put operations
 vim.opt.clipboard = "unnamedplus"
